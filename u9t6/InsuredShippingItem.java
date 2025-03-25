49b87d057d468a7f9e68a2d7771e3d48446fdcce
Andy <andyw63@nycstudents.net>
2025-03-25 11:38:25 -0400

    U9T6

---
+++ u9t6/InsuredShippingItem.java
@@ -0,0 +1,22 @@
+package u9t6;
+
+public class InsuredShippingItem extends ShippingItem {
+    private double insuredAmount;
+
+    public InsuredShippingItem(double w, double insAmt) {
+        super(w);
+        insuredAmount = insAmt;
+    }
+
+    public double getInsuredAmount() {
+        return insuredAmount;
+    }
+
+    public double getCost() {
+        return super.getCost() + insuredAmount;
+    }
+
+    public void addMoreInsurance(double amt) {
+        insuredAmount += amt;
+    }
+}
